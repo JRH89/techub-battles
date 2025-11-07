7143317c19f08036774aced0583c47b9e95c667d
Jared Hooker <jaredroberthooker@gmail.com>
2025-11-07 15:28:38 -0800

    feat: add image domain configuration for external sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['github.com', 'avatars.githubusercontent.com', 'techub.life', 'battles.techub.life'],
+  },
 };
 
 export default nextConfig;
